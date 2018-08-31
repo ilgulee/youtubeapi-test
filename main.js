dfd9214d70e3d45e8ddc00282fa3b7cc6ef56cbc
Ilgu Lee <iglee2000ca@gmail.com>
2018-08-31 05:27:43 -0400

    Add playListItems

--- main.js
+++ main.js
@@ -147,5 +147,21 @@ function requestVideoPlaylists(playlistId){
     console.log(request);
     request.execute(response=>{
         console.log(response);
+        const playlistItems=response.result.items;
+        if(playlistItems){
+            let output='<br><h4 class="center-align">Latest Videos</h4>';
+            playlistItems.forEach(element => {
+                const videoId=element.snippet.resourceId.videoId;
+                output +=`
+                <div class="col s3">
+                <iframe width="100%" height="auto" src="https://www.youtube.com/embed/${videoId}" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen>
+                </iframe>
+                </div>
+                `;
+            });
+            videoContainer.innerHTML=output;
+        }else{
+            videoContainer.innerHTML='No Uploaded Videos';
+        }
     });
 }
